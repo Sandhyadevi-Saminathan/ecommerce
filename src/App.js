e3767c98884c8cf16145387b5439f65431379dae
Sandhya devi Saminathan <sandhya021999@gmail.com>
2023-11-19 23:37:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Login from './Login';
+import Register from './Register';
+import Portal from './Portal';
+
+import Topbar from './Topbar';
+import Sidebar from './Sidebar';
+import Home from './Home';
+import Grocery from './Grocery';
+import Mobiles from './Mobiles';
+import Fashion from './Fashion';
+import Electronics from './Electronics';
+import Furniture from './Furniture';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path='/portal' element={<Portal />}>
+          <Route path="sidebar" element={<Sidebar />} />
+          <Route path="topbar" element={<Topbar />} />
+          <Route path = 'home' element={<Home/>} />
+          <Route path = 'grocery' element={<Grocery/>} />
+          <Route path = 'mobiles' element={<Mobiles/>} />
+          <Route path = 'fashion' element={<Fashion/>} />
+          <Route path = 'electronics' element={<Electronics/>} />
+          <Route path = 'furniture' element={<Furniture/>} />
+          </Route>
+          {/* <Route path="/forget" element={<Forget/>} />
+          <Route path="/verification/:id" element={<Verification />} />
+          <Route path="/Changepassword/:id" element={<Changepassword/>} /> */}
+        </Routes>
+    
+
+    </BrowserRouter>
+  </div >
   );
 }
 
